6462075d08694c68e9a68172a473266d9f51bb2a
dwesely <dwesely@users.noreply.github.com>
2016-05-20 22:25:51 -0400

    Outline
    Added purpose and general outline

---
+++ songlist.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 20 22:03:45 2016
+
+songlist.py
+
+Purpose: Parse a folder full of tab-separated files containing historical information
+on the use of hymns, to be used as a planning tool to reduce back-to-back usage of
+hymns, but also provide a list of hymns with familiarity to be used as a baseline
+repertoire.
+
+@author: Wesely
+"""
+
+#def song: number, title, set of dates used
+#if there are spelling variations in title, maybe keep a list of referenced titles, sort, and take the median?
+#def getSong(number)
+
+#def date: date, poc, set of songs referenced
+#def getDate(date)
+
+#File management
+
+#Initialize variables
+
+#Loop through all .tsv files
+
+#split recorded songs (typically semicolon or comma separating multiple entries)
+#parse song title and number
+
+#save new song/date
+
+#write to report:
+#Number of song,Song title,Date last used,# Uses in the last 9 weeks,# Uses in the last 52 weeks,Total # uses
